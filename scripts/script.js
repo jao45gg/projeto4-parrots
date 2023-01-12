99a396fdbbcbe5d6f57af99ea7756a6448803477
joao <joaosb004@gmail.com>
2023-01-12 07:57:02 -0300

    Virar carta no click implementado

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-.
+function virarCarta(carta) {
+    carta.children[0].style.cssText =
+    'transform: rotateY(-180deg);';
+    carta.children[1].style.cssText =
+    'transform: rotateY(0deg);';
+}
